e1b758fd1fc54c6142aeea37e9c6fb52947c8118
Seanp0w <seanpowell581@gmail.com>
2023-06-21 11:06:27 +1000

    Commit

--- Assets/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Characters/Player/Scripts/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Weapon weapon;
 
     private Vector2 moveDirection;
+    private mousePosition;
     
     //
     void Start()
@@ -30,13 +32,22 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenWoWorldPoint(Input.mousePosition);
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 
 }
