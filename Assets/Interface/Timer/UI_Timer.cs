5b47189050ce9178e635f85c3051affaef3f94fe
gard-en <gard.en@outlook.com>
2023-06-22 09:22:53 +0930

    TextMeshPro

---
+++ Assets/Interface/Timer/UI_Timer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Timer : MonoBehaviour
+{
+
+	#region ExternalLinks
+
+	#endregion
+
+	#region Attributes
+
+	
+	#endregion
+
+	#region Unity
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+	#endregion
+}
