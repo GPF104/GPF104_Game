45fa7a6cc9bac845f13f24caf13217a20dffd6ca
gard-en <gard.en@outlook.com>
2023-06-21 11:52:39 +0930

    TimeManager

--- Assets/Managers/TimeManager/TimeManager.cs
+++ Assets/Managers/TimeManager/TimeManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	#region ExternalLinks
+
+	GameManager gameManager;
+	UIHandler uiHandler;
+
+	#endregion
+
+	#region Attributes
+
+	private float time = 0;
+	private bool active = false;
+	IEnumerator Timer(float interval)
+	{
+		while (active)
+		{
+			yield return new WaitForSeconds(interval);
+			time += 1;
+		}
+	}
+
+	public void Start(float interval)
+	{
+		StartCoroutine(Timer(interval));
+	}
+
+	public float GetTime()
+	{
+		return time;
+	}
+	#endregion
+
+	#region Unity
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		gameManager = this.GetComponentInParent<GameManager>();
+		uiHandler = this.GetComponentInParent<UIHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#endregion
 }
