d7239e51770df5382636ffc13595c5d509a1fff7
Seanp0w <seanpowell581@gmail.com>
2023-06-24 15:40:09 +1000

    Enemy Movement
    EnemyMovement

--- Assets/Creatures/Enemy/Scripts/EnemyBehaviour.cs
+++ Assets/Creatures/Enemy/Scripts/EnemyBehaviour.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveEnemy(movement);
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
