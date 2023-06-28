c330865d819d8cfe683510db8441198349f73ce1
Seanp0w <seanpowell581@gmail.com>
2023-06-28 13:50:28 +1000

    Ranged Enemy

--- Assets/Creatures/RangedEnemy/RangedEnemy.cs
+++ Assets/Creatures/RangedEnemy/RangedEnemy.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public float shootingDistance = 5f;
+    public float stoppingDistance = 3f;
+
+    public float fireRate;
+    private float timeToFire;
+    public Transform firingPoint;
+
     void Start()
     {
-        
+        player = GameObject.FindObjectOfType<Player>().GetComponent<Transform>();
+        rb = this.GetComponent<Rigidbody2D>();
+        timeToFire = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+
+        if (Vector2.Distance(player.position, transform.position) <= shootingDistance)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (timeToFire <= 0f)
+        {
+            Debug.Log("Shoot");
+            timeToFire = fireRate;
+        }
+        else
+        {
+            timeToFire -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (player != null)
+        {
+            if (Vector2.Distance(player.position, transform.position) >= stoppingDistance)
+                moveEnemy(movement);
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
         
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
