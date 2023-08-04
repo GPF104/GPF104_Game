ebfaf7dbf98d6648788911bc2cec13db752d42c7
Seanp0w <seanpowell581@gmail.com>
2023-08-04 12:34:18 +1000

    UI overhaul

---
+++ Assets/World/Props/Traps/bindi/BindiScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindiScript : MonoBehaviour
+{
+    //for player script to call for damage
+    HealthScript healthscript;
+    public int damage = 2;
+
+    
+}
