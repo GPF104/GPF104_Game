d4b8af0d452e1356936ca098944d051eb746fc59
gard-en <gard.en@outlook.com>
2023-08-13 12:09:36 +0930

    potions and scroll bob anim

--- Assets/Items/Scrolls/ScrollScript.cs
+++ Assets/Items/Scrolls/ScrollScript.cs
@@ -9,6 +9,26 @@ public class ScrollScript : MonoBehaviour
     GameObject blip;
     AudioSource audioSource;
     [SerializeField] AudioClip spawnSFX;
+
+    public float bobSpeed = 1.0f;  // Speed of bobbing motion
+    public float bobHeight = 0.2f; // Height of bobbing motion
+
+    private Vector3 originalPosition;
+
+    IEnumerator Bob()
+    {
+        float elapsedTime = 0.0f;
+
+        while (true)
+        {
+            float newY = originalPosition.y + Mathf.Sin(elapsedTime * bobSpeed) * bobHeight;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+    }
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -21,6 +41,8 @@ public class ScrollScript : MonoBehaviour
             blip = GameObject.FindObjectOfType<GameManager>().uiHandler.uiMap.AddMapElement(BlipType.item);
             GameObject.FindObjectOfType<GameManager>().uiHandler.uiMap.UpdateBlipPosition(blip, this.transform.position);
         }
+        originalPosition = transform.position;
+        StartCoroutine(Bob());
     }
     void OnDestroy()
     {
